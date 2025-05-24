677fda086b54db974a5c33196846566c46aee274
Ivan Tarozzi <itarozzi@gmail.com>
2025-05-24 23:51:18 +0200

    fix include file

--- keyboards/r12_it/config.h
+++ keyboards/r12_it/config.h
@@ -6,11 +6,10 @@
 
 #define OLED_DISPLAY_128X64
 
-#define ENCODER_A_PINS { GP6 }
-#define ENCODER_B_PINS { GP5 }
+// #define ENCODER_A_PINS { GP6 }
+// #define ENCODER_B_PINS { GP5 }
 
-#define ENCODER_RESOLUTION 4
-#define ENCODER_MAP_KEY_DELAY 10
+// #define ENCODER_RESOLUTION 4
+// #define ENCODER_MAP_KEY_DELAY 10
 
 
-#define ENCODER_SWITCH_PINS { GP7 } // to be confirmed
--- keyboards/r12_it/keymaps/2layers/config.h
+++ keyboards/r12_it/keymaps/2layers/config.h
@@ -1,1 +1,10 @@
 
+#pragma once
+
+#define NUMBER_OF_ENCODERS 1
+
+#define ENCODER_A_PINS { GP6 }
+#define ENCODER_B_PINS { GP5 }
+
+#define ENCODER_RESOLUTION 4
+#define ENCODER_MAP_KEY_DELAY 10
