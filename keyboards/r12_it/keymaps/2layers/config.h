0f419bf68c3796997ab32668d9e1aaa29850bb0e
Ivan Tarozzi <itarozzi@gmail.com>
2025-05-24 12:33:00 +0200

    add new custom keyboard r12_it derived from Khor R12, for testing OLED and Encoder

---
+++ keyboards/r12_it/keymaps/2layers/config.h
@@ -0,0 +1,9 @@
+#define ENCODER_A_PINS { GP6 }
+#define ENCODER_B_PINS { GP5 }
+
+#define ENCODER_RESOLUTION 4
+#define ENCODER_MAP_KEY_DELAY 10
+
+
+#define ENCODER_SWITCH_PINS { GP7 } // to be confirmed
+
