0fb5f9ffa1c272eda14fea63af3b84557020c252
Ivan Tarozzi <itarozzi@gmail.com>
2025-05-25 00:39:27 +0200

    encoder switch management

--- keyboards/r12_it/keymaps/2layers/keymap.c
+++ keyboards/r12_it/keymaps/2layers/keymap.c
@@ -12,9 +12,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * Layout physical keys index
      *
-     * ┌────┬────┬────┬────┐
-     * │  0 │  1 │  2 │  3 │
-     * ├────┼────┼────┼────┤ 
+     * ┌────┬────┬────┬────┬──────────────┐
+     * │  0 │  1 │  2 │  3 │ EncoderSwitch│
+     * ├────┼────┼────┼────┼──────────────┤ 
      * │  4 │  5 │  6 │  7 │
      * ├────┼────┼────┼────┤
      * │  8 │  9 │ 10 │ 11 │
@@ -22,13 +22,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [_L1] = LAYOUT_numpad_3x4(
-        KC_P7,   KC_P8,   KC_P9,   MO(_L2),
+        KC_P7,   KC_P8,   KC_P9,   KC_P0,   MO(_L2),
         KC_P4,   KC_P5,   KC_P6,   KC_PDOT,
         KC_P1,   KC_P2,   KC_P3,   KC_PENT
     ),
 
     [_L2] = LAYOUT_numpad_3x4(
-        KC_Q,   KC_W,   KC_E,   KC_R,   
+        KC_Q,   KC_W,   KC_E,   KC_R,   MO(_L2), 
         KC_A,   KC_S,   KC_D,   KC_F,
         KC_Z,   KC_X,   KC_C,   KC_V
     )
